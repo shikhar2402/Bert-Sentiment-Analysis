302030681904b60ceb07a54dda7516d11a133da7
shikhar2402 <46794327+shikhar2402@users.noreply.github.com>
2023-01-16 17:01:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,5 +7,5 @@ VALID_BATCH_SIZE = 64
 EPOCHS = 1
 BERT_PATH = "bert-base-uncased"
 MODEL_PATH = "model.bin"
-TRAINING_FILE = "../IMDB Dataset.csv"
+TRAINING_FILE = "IMDB Dataset.csv"
 TOKENIZER = transformers.BertTokenizer.from_pretrained(BERT_PATH, do_lower_case=True)
